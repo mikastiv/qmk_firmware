b0c50bf698be34794d30062036ba0ea992b60a8c
Mikastiv <mikastiv@outlook.com>
2023-06-27 00:41:30 -0400

    update

--- keyboards/crkbd/rev1/keymaps/mikastiv/keymap.c
+++ keyboards/crkbd/rev1/keymaps/mikastiv/keymap.c
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                        ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                        |--------+--------+--------+--------+--------+--------|
       KC_LALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                        |--------+--------+--------+--------+--------+--------|
@@ -36,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                        ,-----------------------------------------------------.
        KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                        |--------+--------+--------+--------+--------+--------|
-      KC_LALT,   KC_LT,   KC_GT, KC_QUOT, KC_DQUO,    KC_G,                          KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,  KC_DEL, KC_BSPC,
+      KC_LALT,   KC_LT,   KC_GT, KC_QUOT, KC_DQUO,    KC_G,                          KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_LGUI, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                        |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, KC_COLN, KC_QUES,    KC_D,                          KC_HOME, KC_PGUP, KC_PGDN,  KC_END, KC_PSCR, KC_LGUI,
+      KC_LSFT, XXXXXXX, XXXXXXX, KC_COLN, KC_QUES,    KC_D,                          KC_HOME, KC_PGUP, KC_PGDN,  KC_END, KC_PSCR, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|      |--------+--------+--------+--------+--------+--------+--------|
                             LCTL_T(KC_ESC), _______, LSFT_T(KC_SPC),       RSFT_T(KC_ENT), MO(3), KC_RCTL
                                       //`--------------------------'      `--------------------------'
