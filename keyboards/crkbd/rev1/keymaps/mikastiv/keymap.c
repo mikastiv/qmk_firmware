2658d13c3d4b57953df0415ace8761407e5231ba
Mikastiv <mikastiv@outlook.com>
2024-03-05 22:28:36 -0500

    add norman layout

--- keyboards/crkbd/rev1/keymaps/mikastiv/keymap.c
+++ keyboards/crkbd/rev1/keymaps/mikastiv/keymap.c
@@ -14,7 +14,8 @@ enum combos {
 };
 
 enum custom_layers {
-  DEFAULT,
+  QWERTY_DEFAULT,
+  NORMAN_DEFAULT,
   LOWER,
   RAISE,
   FUNC,
@@ -30,21 +31,22 @@ combo_t key_combos[] = {
   [JK_ESC] = COMBO(jk_esc_combo, KC_ESC),
 };
 
-// For DEFAULT layer
 #define OSM_LCTL OSM(MOD_LCTL)
 #define OSM_LALT OSM(MOD_LALT)
 #define OSM_LSFT OSM(MOD_LSFT)
 #define OSL_FUN  OSL(FUNC)
 #define GUI_ENT  GUI_T(KC_ENT)
-#define LOW_TAB  LT(LOWER, KC_TAB)
-#define RSE_BSP  LT(RAISE, KC_BSPC)
 
-// For _GAMING layer
+#define LOW_TAB LT(LOWER, KC_TAB)
+#define RSE_BSP LT(RAISE, KC_BSPC)
+#define TO_DEF_Q TO(QWERTY_DEFAULT)
+#define TO_DEF_N TO(NORMAN_DEFAULT)
+
+// For GAMING layer
 #define MO_LOW MO(GAMING_LOWER)
-#define TO_DEF TO(DEFAULT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [DEFAULT] = LAYOUT(
+  [QWERTY_DEFAULT] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -80,11 +82,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
+  [NORMAN_DEFAULT] = LAYOUT(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB,    KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,                         KC_J,    KC_U,    KC_R,    KC_L, KC_SCLN,  KC_DEL,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+     OSM_LALT,    KC_A,    KC_S,    KC_E,    KC_T,    KC_G,                         KC_Y,    KC_N,    KC_I,    KC_O,    KC_H, KC_UNDS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+     OSM_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_P,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, OSL_FUN,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                         OSM_LCTL, LOW_TAB,  KC_SPC,    KC_RSFT, RSE_BSP, GUI_ENT
+                                      //`--------------------------'  `--------------------------'
+  ),
+
   [FUNC] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
+      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,TO_DEF_Q,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,                      RGB_HUI, RGB_SAI, RGB_VAI, RGB_MOD, RGB_TOG, _______,
+      _______,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,                      RGB_HUI, RGB_SAI, RGB_VAI, RGB_MOD, RGB_TOG,TO_DEF_N,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      RGB_HUD, RGB_SAD, RGB_VAD,RGB_RMOD, QK_BOOT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -94,11 +108,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [GAMING] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,TO_DEF_Q,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_UNDS,
+      KC_LALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,TO_DEF_N,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  TO_DEF,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL,  MO_LOW,  KC_SPC,    KC_RSFT, KC_BSPC, GUI_ENT
                                       //`--------------------------'  `--------------------------'
@@ -149,7 +163,7 @@ static void init_rgb_matrix(void) {
 }
 
 void suspend_wakeup_init_user(void) {
-    layer_state_set(DEFAULT);
+    layer_state_set(QWERTY_DEFAULT);
     init_rgb_matrix();
 }
 
@@ -198,8 +212,11 @@ static void render_layer_state(void) {
 
     const char *msg;
     switch (get_highest_layer(layer_state)) {
-        case DEFAULT:
-            msg = PSTR("Default");
+        case QWERTY_DEFAULT:
+            msg = PSTR("Qwerty");
+            break;
+        case NORMAN_DEFAULT:
+            msg = PSTR("Norman");
             break;
         case LOWER:
             msg = PSTR("Lower");
